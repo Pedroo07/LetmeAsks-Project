7ae16dbda7e189db17e1b666707ee53a873d20d8
Pedroo07 <pedrohenriquemoalive@gmail.com>
2025-07-10 21:29:34 -0300

    first commit

---
+++ server/src/server.ts
@@ -0,0 +1,13 @@
+import fastify from "fastify";
+import { serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
+import fastifyCors from "@fastify/cors";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+app.register(fastifyCors, {
+  origin: "http://localhost:5173",
+});
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+app.listen({
+  port: 3333,})
